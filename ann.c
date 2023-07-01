0127123e8b4052ba62cc768e0dbf5b2478b453ae
ninorepo <nino.verify@gmail.com>
2023-07-01 23:49:47 +0700

    Adding import export functions, used for store the object to text file, or load text file into ANN object

--- ann.c
+++ ann.c
@@ -11,6 +11,49 @@ void __print_array(const char *label, double *arr, int size)
 	printf("}\n");
 }
 
+void __ann_print(struct ann *ann)
+{
+	int i;
+	printf("weights\t");
+	for (i = 0; i < ann->weight_size; i++)
+	{
+		printf("%f  ", ann->weight[i]);
+	}
+	printf("\n");
+	printf("inputs\t");
+	for (i = 0; i < ann->input_size; i++)
+	{
+		printf("%f  ", ann->input[i]);
+	}
+	printf("\n");
+
+	printf("hidden\t");
+	for (i = 0; i < ann->hidden_size; i++)
+	{
+		printf("%f  ", ann->hidden[i]);
+	}
+	printf("\n");
+
+	printf("output\t");
+	for (i = 0; i < ann->output_size; i++)
+	{
+		printf("%f  ", ann->output[i]);
+	}
+	printf("\n");
+	printf("input-size=%d\n", ann->input_size);
+	printf("hidden-size=%d\n", ann->hidden_size);
+	printf("output-size=%d\n", ann->output_size);
+	printf("weight-size=%d\n", ann->weight_size);
+	printf("learning-rate=%lf\n", ann->lrate);
+	printf("error=%lf\n", ann->error);
+	printf("max-epoch=%d\n", ann->max_epoch);
+	printf("target=%lf\n", ann->target);
+	printf("min-norm=%d\n", ann->min_norm);
+	printf("max-norm=%d\n", ann->max_norm);
+	printf("hidden-func-type=%d\n", ann->hidden_func_type);
+	printf("output-func-type=%d\n", ann->output_func_type);
+}
+
 int __ann_tanh(double *buffer, int size)
 {
 	int i;
@@ -86,8 +129,9 @@ int ann_output_func(struct ann *ann, enum ann_func func)
 		ann->output_func = __ann_softmax;
 		break;
 	default:
-		break;
+		return ANN_INVALID_FUNC;
 	}
+	ann->output_func_type = func;
 	return 0;
 }
 
@@ -114,8 +158,9 @@ int ann_hidden_func(struct ann *ann, enum ann_func func)
 		ann->hidden_func = __ann_softmax;
 		break;
 	default:
-		break;
+		return ANN_INVALID_FUNC;
 	}
+	ann->hidden_func_type = func;
 	return 0;
 }
 
@@ -124,11 +169,10 @@ double __coin_toss(double number)
 {
 	int r = 0;
 	double toss = 0.0;
-	r = (rand()+1) % 2;
-	toss = (r > 0) ? 1 : -1;
+	r = (rand() + 1) % 2;
+	toss = (r > 0) ? 1.0 : -1.0;
 	return number * toss;
 }
-
 // generate random values using __coin_toss,store the result to roulette array
 int __ann_spin_roulette(struct ann *ann)
 {
@@ -149,12 +193,16 @@ int __ann_default_setup(struct ann *ann)
 	ann->error = DBL_MAX;
 	ann->lrate = 0.2;
 	ann->target = 0.03;
+	ann->epoch = 0;
 	ann->max_epoch = 1000;
 	ann->hidden_func = __ann_relu;
 	ann->output_func = __ann_sigmoid;
+	ann->hidden_func_type = ANN_RELU;
+	ann->output_func_type = ANN_SIGMOID;
 	ann->seed = time(NULL);
 	ann->min_norm = 0;
 	ann->max_norm = 1000;
+	ann->debug = 0;
 	//	ann->timeout = 10000;
 	__ann_spin_roulette(ann);
 	return 0;
@@ -363,45 +411,38 @@ int ann_teach(struct ann *ann, double *inputset, double *outputset, int record_n
 	while (1)
 	{
 		// break if target error has been achieved, or max epoch reached
-		if ((ann->error <= ann->target) || (epoch >= ann->max_epoch))
+		if (ann->error <= ann->target)
+			break;
+		if(ann->epoch >= ann->max_epoch)
 			break;
 
 		// if one epoch has been reached
 		if (loops >= record_num)
 		{
-			++epoch;
-			printf("epoch: %d\t", epoch);
+			++ann->epoch;
+			if(ann->debug)			
+				printf("epoch: %d\t", ann->epoch);
 			//e = e/(record_num*ann->output_size);
 			i = 0;
 			j = 0;
 			loops = 0;
-
 			// code for update and reset weights here ...
 			if ((e < ann->error)) // if new_error smaller than old_error
 			{
 				// update
 				__ann_update(ann);
 				ann->error = e;
-				//				reset_timeout = ann->timeout;
-				printf("e: %f\n", ann->error);
+				if(ann->debug)
+					printf("e: %f\n", ann->error);
 			}
 			else
 			{
 				__ann_reset(ann);		  // reset or revert weights back to previous values
 				__ann_spin_roulette(ann); // spin roulette, roulette is array of values used for increasing or decreasing weights
 				__ann_update(ann);		  // update weights
-										  //				--reset_timeout;
-				printf("e: %f\n", ann->error);
-				//printf("reset\n");
+				if(ann->debug)				
+					printf("e: %f\n", ann->error);
 			}
-			//__print_array("weight", ann->weight, ann->weight_size);
-			/*
-			if (reset_timeout <= 0)
-			{
-				printf("local minima identified: revert error back");
-				ann->error += 20;
-			}
-			*/
 			e = 0.0;
 		}
 
@@ -418,7 +459,112 @@ int ann_teach(struct ann *ann, double *inputset, double *outputset, int record_n
 
 		//loop counter
 		++loops;
-		//printf("%f\n", e);
 	}
 	return 0;
+}
+
+int ann_export(struct ann *ann, char *filepath)
+{
+	FILE *f = fopen(filepath, "w");
+
+	if (!f)
+		return ANN_FILE_FAILURE;
+	fprintf(f, "input-size=%d\r\n", ann->input_size);
+	fprintf(f, "hidden-size=%d\r\n", ann->hidden_size);
+	fprintf(f, "output-size=%d\r\n", ann->output_size);
+	fprintf(f, "weight-size=%d\r\n", ann->weight_size);
+	fprintf(f, "learning-rate=%f\r\n", ann->lrate);
+	fprintf(f, "error=%f\r\n", ann->error);
+	fprintf(f, "max-epoch=%d\r\n", ann->max_epoch);
+	fprintf(f, "target=%f\r\n", ann->target);
+	fprintf(f, "min-norm=%d\r\n", ann->min_norm);
+	fprintf(f, "max-norm=%d\r\n", ann->max_norm);
+	fprintf(f, "hidden-func-type=%d\r\n", (int)ann->hidden_func_type);
+	fprintf(f, "output-func-type=%d\r\n", (int)ann->output_func_type);
+	fprintf(f, "#weight-start\r\n");
+
+	int i;
+	for (i = 0; i < ann->weight_size; i++)
+	{
+		fprintf(f, "%f\r\n", ann->weight[i]);
+	}
+	fprintf(f, "#weight-end\r\n");
+	fclose(f);
+	return 0;
+}
+
+struct ann *ann_import(const char *filepath)
+{
+	char str[35] = {0};
+	int input_size = 0;
+	int hidden_size = 0;
+	int output_size = 0;
+	int weight_size = 0;
+	double lrate = 0.0;
+	double error = 0.0;
+	int max_epoch = 0;
+	double target = 0.0;
+	int min_norm = 0;
+	int max_norm = 0;
+	enum ann_func hidden_func_type = ANN_RELU;
+	enum ann_func output_func_type = ANN_RELU;
+	int weight_cursor = 0;
+	int weight_start = 0;
+	double weight = 0.0;
+
+	struct ann *ann = NULL;
+	FILE *f = fopen(filepath, "r");
+
+	if (!f)
+		return NULL;
+
+	while (fgets(str, 35, f) != NULL)
+	{
+		sscanf(str, "input-size=%d", &input_size);
+		sscanf(str, "hidden-size=%d", &hidden_size);
+		sscanf(str, "output-size=%d", &output_size);
+		sscanf(str, "weight-size=%d", &weight_size);
+		sscanf(str, "learning-rate=%lf", &lrate);
+		sscanf(str, "error=%lf", &error);
+		sscanf(str, "max-epoch=%d", &max_epoch);
+		sscanf(str, "target=%lf", &target);
+		sscanf(str, "min-norm=%d", &min_norm);
+		sscanf(str, "max-norm=%d", &max_norm);
+		sscanf(str, "hidden-func-type=%d", (int*)&hidden_func_type);
+		sscanf(str, "output-func-type=%d", (int*)&output_func_type);
+
+		// create neural network when all requirements have been retrieved: input size,hidden size,output size
+		if (output_size && !ann)
+		{
+			ann = (struct ann *)malloc(sizeof(struct ann));
+			*ann = ann_create(input_size - 1, hidden_size - 1, output_size);
+		}
+		// detect weight start tag to extract weights
+		if (strcmp(str, "#weight-start\r\n") == 0)
+		{
+			weight_start = 1;
+			fgets(str, 35, f); // skip #weight-start tag in *.ann file
+		}
+		if (strcmp(str, "#weight-end\r\n") == 0)
+			weight_start = 0;
+
+		if (weight_start)
+		{
+			sscanf(str, "%lf", &weight);
+			ann->weight[weight_cursor] = weight;
+			++weight_cursor; //weight_cursor is used as index of weight array in the ann object
+		}
+	}
+	// transfer datas into ANN object
+	ann->lrate = lrate;
+	ann->error = error;
+	ann->max_epoch = max_epoch;
+	ann->target = target;
+	ann->min_norm = min_norm;
+	ann->max_norm = max_norm;
+	ann->hidden_func_type = (enum ann_func)hidden_func_type;
+	ann->output_func_type = (enum ann_func)output_func_type;
+	
+	fclose(f);
+	return ann;
 }
--- ann.h
+++ ann.h
@@ -1,12 +1,12 @@
 #ifndef ANN_H
 #define ANN_H
 
-#include "stdio.h"
-#include "string.h"	//memcpy
-#include "stdlib.h"	// malloc, rand()
+#include "string.h" 	//memcpy, strcmp
+#include "stdlib.h" 	// malloc
 #include "time.h"	// time(NULL)
 #include "math.h"	// tanh
 #include "float.h"	//DBL_MAX
+#include "stdio.h"	//printf
 
 enum ann_func
 {
@@ -21,7 +21,9 @@ enum ann_err
 {
 	ANN_OK,
 	ANN_INVALID_OBJ,
-	ANN_INVALID_VAL
+	ANN_INVALID_VAL,
+	ANN_FILE_FAILURE,
+	ANN_INVALID_FUNC
 };
 
 struct ann
@@ -40,43 +42,50 @@ struct ann
 	//int bias_size;
 	int roulette_size;
 
-	double error;	  // latest error value
-	double lrate;	  // learning rate
-	double max_epoch; // max epoch
-	double target;	  // target error
+	double error;  // latest error value
+	double lrate;  // learning rate
+	int epoch;
+	int max_epoch; // max epoch
+	double target; // target error
 
+	enum ann_func hidden_func_type;
+	enum ann_func output_func_type;
 	int (*hidden_func)(double *array, int size);
 	int (*output_func)(double *array, int size);
 
 	int seed;
 	//	int timeout;
 
-	double min_norm;
-	double max_norm;
+	int min_norm;
+	int max_norm;
+
+	int debug;
 };
 
 void __print_array(const char *label, double *arr, int size);
-int __ann_tanh(double *buffer, int size);
+void __ann_print(struct ann *ann);;
 int __ann_linear(double *buffer, int size);
 int __ann_softmax(double *buffer, int size);
 int __ann_sigmoid(double *buffer, int size);
 int __ann_relu(double *buffer, int size);
 int ann_output_func(struct ann *ann, enum ann_func func);
-int ann_hidden_func(struct ann *ann, enum ann_func func);	
-double __coin_toss(double number);				
-int __ann_spin_roulette(struct ann *ann);	// generate random values between -lrate to +lrate
-int __ann_default_setup(struct ann *ann);	// generate random values using __coin_toss,store the result to roulette array
-int __ann_input_x_weight(struct ann *ann);	// doing cross product between input layer to the adjescent weights
-int __ann_hidden_x_weight(struct ann *ann);	// doing cross product between hidden layer to the adjescent weights
+int ann_hidden_func(struct ann *ann, enum ann_func func);
+double __coin_toss(double number);				// generate random values between -lrate to +lrate
+int __ann_spin_roulette(struct ann *ann);			// generate random values using __coin_toss,store the result to roulette array
+int __ann_default_setup(struct ann *ann);
+int __ann_input_x_weight(struct ann *ann);			// doing cross product between input layer to the adjescent weights
+int __ann_hidden_x_weight(struct ann *ann);			// doing cross product between hidden layer to the adjescent weights
 int __ann_init_weight(struct ann *ann, double *array, int size);
-int __ann_update(struct ann *ann);		// basically this function add weights to the roulette array
-int __ann_reset(struct ann *ann);		// revert the value of weights to the previous values,cancel out weight change
+int __ann_update(struct ann *ann);				// basically this function add weights to the roulette array
+int __ann_reset(struct ann *ann);				// revert the value of weights to the previous values,cancel out weight change
 double __ann_eval_loss(struct ann *ann, double *outputset);	// loss function calculation, return the total error of an output array/nodes
-int ann_add_input(struct ann *ann, double *input);	// add input to the ANN in normalized form
-int ann_get_output(struct ann *ann, double *buffer);	// get output and denormalize the output
+int ann_add_input(struct ann *ann, double *input);		// add input to the ANN in normalized form
+int ann_get_output(struct ann *ann, double *buffer);		// get output and denormalize the output
 struct ann ann_create(int in, int hid, int out);
 int ann_destroy(struct ann *ann);
-int ann_forward(struct ann *ann);	// doing feedforward
+int ann_forward(struct ann *ann);				// doing feedforward
 int ann_teach(struct ann *ann, double *inputset, double *outputset, int record_num);
+int ann_export(struct ann *ann, char *filepath);		// save ann object datas into text file
+struct ann *ann_import(const char *filepath);			// create ann object based on previously saved data
 
 #endif
